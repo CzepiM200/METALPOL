cb0754d153ca09288e7039ff728df5375beaf354
CzepiM200 <czepimm200@gmail.com>
2022-01-15 15:58:05 +0100

    Add gallery

---
+++ js/gallery.js
@@ -0,0 +1,15 @@
+const images = document.querySelectorAll(".gallery__image-item")
+const galleryZoom = document.querySelector(".gallery-zoom")
+const galleryZoomImage = document.querySelector(".gallery-zoom-image")
+
+galleryZoom.addEventListener('click', () => {
+    console.log(galleryZoom)
+    galleryZoom.style.display = 'none'
+})
+
+images.forEach(element => {
+    element.addEventListener('click', () => {
+        galleryZoom.style.display = 'flex'
+        galleryZoomImage.setAttribute('src', element.src)
+    });
+});
